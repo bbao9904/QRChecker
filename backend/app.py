b133410ad34aae2484cecb05c13c76249044441d
bbao9904 <BaoHTCE181779@fpt.edu.vn>
2025-10-23 19:34:36 +0700

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -474,12 +474,7 @@ def is_strong_password(pw: str) -> bool:
     )
 
 if __name__ == "__main__":
-    # Chạy local (dev) thì mới dùng app.run()
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-
-#if __name__ == "__main__":
- #   app.run(host="0.0.0.0", port=5000, ssl_context='adhoc')
+    app.run(host="0.0.0.0", port=5000, ssl_context='adhoc')
 
     # ...existing code...
 
